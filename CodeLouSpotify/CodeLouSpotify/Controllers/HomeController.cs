55563f680476d404a1395578c2fcad58e9221ff7
amwebdev86 <amwebdev86@outlook.com>
2020-07-20 22:56:12 -0400

    Added Authorization Code

--- CodeLouSpotify/CodeLouSpotify/Controllers/HomeController.cs
+++ CodeLouSpotify/CodeLouSpotify/Controllers/HomeController.cs
@@ -6,23 +6,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CodeLouSpotify.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace CodeLouSpotify.Controllers
 {
     public class HomeController : Controller
     {
+        SpotifyUser user = new SpotifyUser();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+      
 
         public IActionResult Index()
         {
+            
             return View();
         }
-
+        public IActionResult AuthorizeUser()
+        {
+            return Redirect(user.Authorize());
+        }
+     
         public IActionResult Privacy()
         {
             return View();
@@ -32,6 +43,38 @@ namespace CodeLouSpotify.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code">code returned from spotify</param>
+        /// <returns></returns>
+        [Route("/callback")]
+        public IActionResult Callback(string code)
+        {
+            string responseString = string.Empty;
+
+            if(code == null)
+            {
+                return NotFound();
+            }
+            using(HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", 
+                    Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(user.ClientId + ":" + user.ClientSecret)));
+                FormUrlEncodedContent formContent = new FormUrlEncodedContent(
+                        new[]
+                        {
+                            new KeyValuePair<string,string>("code", code),
+                            new KeyValuePair<string,string>("redirect_uri", user.CallbackUri),
+                            new KeyValuePair<string, string>("grant_type", "authorization_code")
+                        });
+                var response = client.PostAsync("https://accounts.spotify.com/api/token", formContent).Result;
+                var responseContent = response.Content;
+                responseString = responseContent.ReadAsStringAsync().Result;
+
+            }
+            return View();
         }
     }
 }
---
+++ CodeLouSpotify/CodeLouSpotify/Data/SpotifyUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLouSpotify.Models
+{
+    public class SpotifyUser
+    {
+        public string ClientId { get; private set; } 
+        public string ClientSecret { get; private set; } 
+        public string CallbackUri { get; set; } = "https://localhost:44363/callback";
+        public SpotifyUser(string clientId, string clientSecret)
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+        public SpotifyUser()
+        {
+            ClientId = "9f6e8509f8b34c5bb57f93f3561e705a";
+            ClientSecret = "fee3b46e6ef944a494bd49e43b60420d";
+        }
+
+        public string Authorize()
+        {
+            var qb = new QueryBuilder();
+            qb.Add("response_type", "code");
+            qb.Add("client_id", ClientId);
+            qb.Add("scope", "user-read-private user-read-email");
+            qb.Add("redirect_uri", CallbackUri);
+            return $@"https://accounts.spotify.com/authorize{qb.ToQueryString()}";
+        }
+    }
+}
