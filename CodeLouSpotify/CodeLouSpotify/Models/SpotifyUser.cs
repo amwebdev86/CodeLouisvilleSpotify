f308f6f02de91af5caacc33984e1ccfec0ba6f12
amwebdev86 <amwebdev86@outlook.com>
2020-07-20 21:03:17 -0400

    Init Commit

---
+++ CodeLouSpotify/CodeLouSpotify/Models/SpotifyUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeLouSpotify.Models
+{
+    public class SpotifyUser
+    {
+        public string ClientId { get; private set; } = "9f6e8509f8b34c5bb57f93f3561e705a";
+        public string ClientSecret { get; private set; } = "";
+        public string CallbackUri { get; set; } = "https://localhost:44363/callback";
+    }
+}
